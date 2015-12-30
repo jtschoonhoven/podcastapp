e221941c91f779e68447299f0c646b8358f47f21
jtschoonhoven <jschoonhoven@lyft.com>
2015-12-29 22:16:47 -0800

    player style improvement

--- app/components_jsx/container/player.jsx
+++ app/components_jsx/container/player.jsx
@@ -11,11 +11,14 @@ export default class Player extends React.Component {
             <div className="player">
                 <nav className="navbar navbar-default">
                     <div className="container">
-                        <button type="button" className="btn btn-default btn-lg navbar-btn">
+                        <button type="button" className="btn btn-link btn-lg navbar-btn navbar-left">
                             <span className="glyphicon glyphicon-play" aria-hidden="true"></span>
                         </button>
-                        <button type="button" className="btn btn-default btn-lg navbar-btn">
+                        <button type="button" className="btn btn-link btn-lg navbar-btn navbar-left">
                             <span className="glyphicon glyphicon-step-forward" aria-hidden="true"></span>
+                        </button>
+                        <button type="button" className="btn btn-link btn-lg navbar-btn navbar-left">
+                            {'Nope — OK'}
                         </button>
                     </div>
                 </nav>
