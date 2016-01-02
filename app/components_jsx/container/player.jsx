3ca09a8fb9f61069005a9da19263e1dff7b3798b
jtschoonhoven <jschoonhoven@lyft.com>
2016-01-01 19:44:49 -0800

    play and pause streaming audio

--- app/components_jsx/container/player.jsx
+++ app/components_jsx/container/player.jsx
@@ -2,13 +2,37 @@ import React from 'react';
 import {Link} from 'react-router';
 
 export default class Player extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            media: {
+                playing: false,
+                pause: () => {},
+                play: () => {}
+            }
+        };
+    }
+
+    componentDidMount() {
+        const media = {
+            playing: false,
+            play: () => document.getElementById('media').play(),
+            pause: () => document.getElementById('media').pause()
+        };
+        this.setState({media: media});
+    }
+
     render() {
         return (
             <div className="player">
                 <nav className="navbar navbar-default">
                     <div className="container">
-                        <button type="button" className="btn btn-link btn-lg navbar-btn navbar-left">
+                        <audio id="media" style={{display: "none"}} controls autoPlay src={`/api/v0/media/${this.props.episodeId}`} />
+                        <button type="button" className="btn btn-link btn-lg navbar-btn navbar-left" onClick={this.state.media.play}>
                             <span className="glyphicon glyphicon-play" aria-hidden="true"></span>
+                        </button>
+                        <button type="button" className="btn btn-link btn-lg navbar-btn navbar-left" onClick={this.state.media.pause}>
+                            <span className="glyphicon glyphicon-pause" aria-hidden="true"></span>
                         </button>
                         <button type="button" className="btn btn-link btn-lg navbar-btn navbar-left">
                             <span className="glyphicon glyphicon-step-forward" aria-hidden="true"></span>
@@ -16,7 +40,6 @@ export default class Player extends React.Component {
                         <button type="button" className="btn btn-link btn-lg navbar-btn navbar-left">
                             {this.props.title}
                         </button>
-                        <audio controls src="/api/v0/media/1" />
                     </div>
                 </nav>
             </div>
