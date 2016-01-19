b9076afebdc64ce6f3e827836d4531d5da828039
jtschoonhoven <jschoonhoven@lyft.com>
2016-01-18 22:01:04 -0800

    faster builds

--- gulpfile.js
+++ gulpfile.js
@@ -9,32 +9,61 @@ const sass = require('gulp-sass');
 const babel = require('gulp-babel');
 const header = require('gulp-header');
 const gutil = require('gulp-util');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const envify = require('envify/custom');
+const package_json = require('./package');
 
 /**
-* Top level gulp commands to build and watch.
-*/
+ * Pull context from env and config without exposing all config JSON
+ */
+const ENV = require('./config').constants.ENV;
+const env = process.env[ENV] || 'development';
+
+/**
+ * 3rd party modules to bundle for browser.
+ */
+const BROWSER_GLOBALS = [
+    'history/lib/createBrowserHistory',
+    'react',
+    'react-dom',
+    'react-redux',
+    'react-router',
+    'redux',
+    'redux-simple-router',
+    'request'
+];
+
+/**
+ * Top level gulp commands to build and watch.
+ */
 gulp.task('default', ['build', 'watch']);
-gulp.task('build', ['bundle', 'style', 'vendor']);
-gulp.task('watch', ['watch:jsx', 'watch:bundle', 'watch:style']);
+gulp.task('build', ['bundle', 'style', 'vendor', 'assets']);
+gulp.task('watch', ['watch:jsx', 'watch:bundle', 'watch:style', 'watch:vendor']);
 
 /**
-* Browserify and bundle.
-*/
+ * Browserify and bundle.
+ */
 gulp.task('bundle', ['jsx'], function() {
-    const stream = fs.createWriteStream("./public/javascripts/bundle.js");
-    browserify("./app/main.js", {debug: true})
-        .transform(babelify.configure({
+    const outFile = fs.createWriteStream("./public/javascripts/bundle.js");
+    const b = browserify("./app/main.js", {debug: true});
+
+    // Excludes 3rd party modules in package.json.
+    for (const vendor_module in package_json.dependencies) {
+        try { b.external(vendor_module); }
+        catch (e) { console.warning(`${vendor_module} from package.json not found in node_modules`); }
+    }
+
+    b.transform(babelify.configure({
             nonStandard: true,
             compact: false,
             comments: false,
             sourceMaps: true
-        }))
-        .bundle()
-        .pipe(stream)
-        .on('end', () => {
-            stream.end();
-            gutil.beep();
-        });
+        }));
+
+    b.transform(envify({[ENV]: env}));
+
+    b.bundle().pipe(outFile);
 });
 
 gulp.task('watch:bundle', function () {
@@ -46,8 +75,37 @@ gulp.task('watch:bundle', function () {
 });
 
 /**
-* Compile JSX in app/components_jsx and put to app/components.
-*/
+ * Bundle 3rd party dependencies.
+ */
+gulp.task('vendor', function() {
+    const b = browserify({debug: false});
+
+    BROWSER_GLOBALS.forEach(vendor_module => {
+        b.require(vendor_module);
+    });
+
+    b.bundle()
+        .pipe(source('vendor.js'))
+        .pipe(buffer())
+        // .pipe(uglify())
+        .pipe(gulp.dest('./public/javascripts'));
+});
+
+gulp.task('watch:vendor', function() {
+    gulp.watch(["./package.json"], ['vendor']);
+});
+
+gulp.task('watch:bundle', function () {
+    gulp.watch([
+        './app/**/*.js',
+        '!./app/components/**/*.js',
+        '!./app/components_jsx/**/*.js'
+    ], ['bundle']);
+});
+
+/**
+ * Compile JSX in app/components_jsx and put to app/components.
+ */
 gulp.task("jsx", function() {
   return gulp.src("./app/components_jsx/**/*.js")
     .pipe(babel({plugins: ['transform-es2015-modules-commonjs', 'transform-react-jsx']}))
@@ -59,10 +117,9 @@ gulp.task('watch:jsx', function() {
     gulp.watch(["./app/components_jsx/**/*.js"], ['bundle']);
 });
 
-
 /**
-* Compile SASS and put to public/stylesheets.
-*/
+ * Compile SASS and put to public/stylesheets.
+ */
 gulp.task('style', function () {
     gulp.src('./style.sass')
         .pipe(sass().on('error', sass.logError))
@@ -74,9 +131,9 @@ gulp.task('watch:style', function () {
 });
 
 /**
-* Bundle styles and fonts from 3rd parties.
-*/
-gulp.task('vendor', function(){
+ * Add assets from bootstrap.
+ */
+gulp.task('assets', function(){
     gulp.src('./bower_components/bootstrap/dist/css/bootstrap.min.css')
         .pipe(gulp.dest('./public/stylesheets/'));
 
